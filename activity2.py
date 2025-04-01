e00ab9159725bd984e3f2c020a214acbb47f0852
AathithanN <Aathithan.navaneetharaja@gmail.com>
2025-04-01 19:58:46 +0100

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,21 @@
+tuple1=()
+tuple2=(8,7,10,11,1,3)
+tuple3=(8, 8.5, 3.97, 27.978)
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+print(tuple2[0])
+print(tuple3[3])
+
+tup1=(9.7, 7.86,"hi", (7,8,10))
+print(tup1[3][1])
+print(tup1[0:2])
+print("\n")
+
+
+tup2=(5.78,3, 98, 28.9)
+for i in tup2:
+    print(i)
+    
+print(tup2)    
